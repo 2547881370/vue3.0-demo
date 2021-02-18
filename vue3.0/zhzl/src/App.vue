699c196332ec51a72134545a4e0d6b9f84258c20
xuhao <2547881370@qq.com>
2021-02-18 17:38:23 +0800

    学习3

---
+++ vue3.0/zhzl/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <ColumnList :list='list'></ColumnList>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import "bootstrap/dist/css/bootstrap.min.css"
+import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+const columnList: ColumnProps[] = [
+  {
+    id: 1,
+    title: "test1",
+    avatar: "https://pic.cnblogs.com/avatar/1833768/20191023180526.png",
+    description: "内容",
+  },
+  {
+    id: 2,
+    title: "test2",
+    avatar: "https://pic.cnblogs.com/avatar/1833768/20191023180526.png",
+    description: "内容",
+  },
+  {
+    id: 3,
+    title: "test3",
+    avatar: "https://pic.cnblogs.com/avatar/1833768/20191023180526.png",
+    description: "内容",
+  },
+  {
+    id: 4,
+    title: "test4",
+    avatar: "https://pic.cnblogs.com/avatar/1833768/20191023180526.png",
+    description: "内容",
+  },
+];
+export default defineComponent({
+  name: "App",
+  components: {
+    ColumnList,
+  },
+  setup(){
+    return {
+      list : columnList
+    }
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3.0/zhzl/src/components/ColumnList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ColumnList">
+    <div v-for='item in list' :key='item.id'>
+      <img :src="item.avatar" >
+      <h3>{{item.title}}</h3>
+      <p>{{item.description}}</p>
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent, PropType } from "vue";
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
+export default defineComponent({
+  name: "ColumnList",
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style>
+</style>
